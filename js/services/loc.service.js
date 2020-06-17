f94f6087ee0828e17948fd4972101402cf1e1458
yashaba <yashabazovner@gmail.com>
2020-06-17 17:28:41 +0300

    fixing loc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,48 +1,28 @@
-console.log('Main!');
+export const locService = {
+    getLocs,
+    getPosition
+}
+var locs = [{ lat: 11.22, lng: 22.11 }]
 
-import { locService } from './services/loc.service.js'
-import { mapService } from './services/map.service.js'
-
-
-locService.getLocs()
-    .then(locs => console.log('locs', locs))
-
-window.onload = () => {
-    mapService.initMap()
-        .then(() => {
-
-            mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-        })
-        .catch(console.log('INIT MAP ERROR'));
-
-    locService.getPosition()
-        .then(pos => {
-
-            console.log('User position is:', pos.coords);
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
+function getLocs() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(locs);
+        }, 2000)
+    });
 }
 
-document.querySelector('.btn').addEventListener('click', (ev) => {
-    console.log('Aha!', ev.target);
-    mapService.panTo(35.6895, 139.6917);
+
+function getPosition() {
+    console.log('Getting Pos');
+
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
 
 
-})
 
 
 
 
-document.querySelector('.btn-my-location').addEventListener('click', (ev) => {
-    console.log('Aha!', ev.target);
-    let currPos = locService.getPosition()
-    currPos
-        .then(loc => {
-            mapService.panTo(loc.latitude, loc.longtitude)
-            console.log(loc)
-        })
-
-
-})
